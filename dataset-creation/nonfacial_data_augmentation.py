675ce31881d525f6a5c6f4ed2d49dcc29bc54593
tusharparimi <tushar.parimi@gmail.com>
2023-04-09 20:58:53 -0400

    agumented non facial images to increase the number of data

---
+++ dataset-creation/nonfacial_data_augmentation.py
@@ -0,0 +1,53 @@
+import cv2
+import os
+import numpy as np
+
+def file_paths(folder_path):
+    f_paths=[]
+    for file in os.listdir(folder_path):
+        eachfile_path=os.path.join(folder_path,file)
+        if os.path.isfile(eachfile_path):
+            f_paths.append(eachfile_path)
+    return f_paths
+
+def get_rois(img,newh,neww,n):
+    (h,w)=img.shape[:2]
+    print(h,w)
+    roi_list=[]
+    x_stride = int(w /(n))
+    y_stride = int(h /(n))
+    if n<= (int((h-newh)/y_stride)+1)*(int((w-neww)/x_stride)+1):
+        x_starts = np.arange(0, w, x_stride)[:int(np.sqrt(n))+1]
+        y_starts = np.arange(0, h, y_stride)[:int(np.sqrt(n))+1]
+        print(len(x_starts),len(y_starts))
+        for y in y_starts:
+            for x in x_starts:
+                roi_list.append(img[y:y+newh,x:x+neww])
+        return roi_list
+    else:
+        print("Error: cannot extract {} rois from image shape ({},{}) with spread out strides".format(n,h,w))
+        return []
+    
+
+
+if __name__ == "__main__":
+
+    image_paths=file_paths("C:\\Users\\tusha\\Downloads\\iccv09Data.tar\\iccv09Data\\iccv09Data\\images")
+    new_dir_path=os.path.join("C:\\Users\\tusha\\OneDrive\\Desktop", "NON_facial_images")
+    if not os.path.exists(new_dir_path):
+        os.mkdir(new_dir_path)
+    print(len(image_paths))
+    i=0
+    for each in image_paths:
+        img=cv2.imread(each)
+        roi_list=get_rois(img,128,128,14)
+        for j in range(i,i+len(roi_list)):
+            cv2.imwrite(new_dir_path+"\\"+str(j)+".png",roi_list[j-i])
+        i=j+1
+            
+    
+    print("DONE")
+    
+
+
+
