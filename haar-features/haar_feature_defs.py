6d6eee6f352a59874e2b7651507bd60e0b735062
[Tushar Parimi] <tushar.parimi@gmail.com>
2024-02-26 05:29:50 -0800

    added 3 boxed vert haar feature def

--- haar-features/haar_feature_defs.py
+++ haar-features/haar_feature_defs.py
@@ -4,6 +4,12 @@ import sys
 sys.path.insert(1,'C:\\Users\\tusha\\Documents\\Projects\\face-detection\\facial-features')
 from integral_image import integral_image, block_sum
 
+"""
+Early Haar featuress by Viola and Jones (look at Haar feature tips issue #1)
+"""
+#------------------------------------------------------------------------------------------------------------------------------------------------
+
+#(2)
 def rect2_haar_hort(gray_img,lenh,lenw,stride_h=1,stride_w=1):
     feature_dict={}
     (h,w)=gray_img.shape
@@ -17,6 +23,7 @@ def rect2_haar_hort(gray_img,lenh,lenw,stride_h=1,stride_w=1):
             feature_dict['rect2_hort_'+str(lenh)+'x'+str(lenw)+'_'+'('+str(i)+','+str(j)+')']=res
     return feature_dict
 
+#(1)
 def rect2_haar_vert(gray_img,lenh,lenw,stride_h=1,stride_w=1):
     feature_dict={}
     (h,w)=gray_img.shape
@@ -30,6 +37,7 @@ def rect2_haar_vert(gray_img,lenh,lenw,stride_h=1,stride_w=1):
             feature_dict['rect2_vert_'+str(lenh)+'x'+str(lenw)+'_'+'('+str(i)+','+str(j)+')']=res
     return feature_dict
 
+#(3)
 def rect3_haar_hort(gray_img,lenh,lenw,stride_h=1,stride_w=1):
     feature_dict={}
     (h,w)=gray_img.shape
@@ -44,6 +52,22 @@ def rect3_haar_hort(gray_img,lenh,lenw,stride_h=1,stride_w=1):
             feature_dict['rect3_hort_'+str(lenh)+'x'+str(lenw)+'_'+'('+str(i)+','+str(j)+')']=res
     return feature_dict
 
+#(3) but vertical divisions
+def rect3_haar_vert(gray_img,lenh,lenw,stride_h=1,stride_w=1):
+    feature_dict={}
+    (h,w)=gray_img.shape
+    for i in range(0,h-lenh+1,stride_h):
+        for j in range(0,w-lenw+1,stride_w):
+            temp=gray_img[i:i+lenh,j:j+lenw]
+            temp_int=integral_image(temp)
+            bsum1=block_sum(temp_int,(0,0),lenw,int(lenh/3))
+            bsum2=block_sum(temp_int,(0,int(lenh/3)),lenw,int(lenh/3))
+            bsum3=block_sum(temp_int,(0,int((lenh*2)/3)),lenw,int(lenh/3))
+            res=bsum2-(bsum1+bsum3)
+            feature_dict['rect3_hort_'+str(lenh)+'x'+str(lenw)+'_'+'('+str(i)+','+str(j)+')']=res
+    return feature_dict
+
+#(4)
 def rect4_haar_diag(gray_img,lenh,lenw,stride_h=1,stride_w=1):
     feature_dict={}
     (h,w)=gray_img.shape
@@ -58,11 +82,11 @@ def rect4_haar_diag(gray_img,lenh,lenw,stride_h=1,stride_w=1):
             res=bsum2+bsum3-(bsum1+bsum4)
             feature_dict['rect4_diag_'+str(lenh)+'x'+str(lenw)+'_'+'('+str(i)+','+str(j)+')']=res
     return feature_dict
-
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
 
-    img=cv2.imread("C:\\Users\\tusha\\OneDrive\\Desktop\\facial_images\\0.png")
+    img=cv2.imread("facial_images\\0.png")
     print(img.shape)
     cv2.imshow("Img",img)
     cv2.waitKey(0)
@@ -92,10 +116,15 @@ if __name__ == "__main__":
         for j in range(3,w24+1):
             features.update(rect3_haar_hort(gray24,i,j,i,j))
     
+    for i in range(3,h24+1):
+        for j in range(1,w24+1):
+            features.update(rect3_haar_vert(gray24,i,j,i,j))
+
     for i in range(2,h24+1):
         for j in range(2,w24+1):
             features.update(rect4_haar_diag(gray24,i,j,i,j))
     
+
     
     print(len(features))
 
