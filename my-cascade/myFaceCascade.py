099d7fa334db81a67a0a69a89bec6d48d97138a7
[Tushar Parimi] <tushar.parimi@gmail.com>
2024-03-01 06:10:52 -0800

    some initial class structure faceDetector

---
+++ my-cascade/myFaceCascade.py
@@ -0,0 +1,37 @@
+
+
+class faceDetector:
+    def __init__(self, img, minWinSize, maxWinSize):
+        self.minWinSize=minWinSize
+        self.maxWinSize=maxWinSize
+        self.img=img
+
+
+
+class faceClassifier:
+    def __init__(self, scale):
+        self.scale=scale
+
+    def compute():
+        pass
+
+
+class faceCascade:
+    def __init__(self):
+        pass
+
+
+class faceCascadeStage:
+    def __init__(self, adaboost_model):
+        self.adaboost_model=adaboost_model
+
+    def classify():
+        pass
+
+
+class faceFeature:
+    def __init__(self):
+        pass
+
+    def getSum():
+        pass
